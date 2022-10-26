835549e195c7f5ab2b5f8f20419b0b5d12562af3
rahim4shanewaz <soumiq444@gmail.com>
2022-10-26 18:44:12 +0600

    checkout page added

--- src/components/checkOut/CheckOut.js
+++ src/components/checkOut/CheckOut.js
@@ -3,7 +3,8 @@ import './CheckOut.css'
 
 const CheckOut = () => {
     return (
-        <div className='flex gap-5 justify-center items-center'>
+        <div>
+            <div className='flex box-container gap-3  justify-center items-center'>
             
             <div class="login-div1">
   
@@ -27,27 +28,50 @@ const CheckOut = () => {
 
 
              
-             <div class="login-div1">
+             <div class="login-div1 p-2">
   
-                <div class="title">Register First</div>
+               
                 
-                <div class="fields1">
-                <p>Enter your present address</p>
-                    <div class="username"><input type="text" class="user-input" placeholder="Present address" /></div>
-                    <p>Enter your Full name</p>
-                    <div class="username"><input type="text" class="user-input" placeholder="Full name" /></div>
-                    <p>Enter your Mobile number</p>
-                    <div class="username"><input type="text" class="user-input" placeholder="mobile number" /></div>
-                    <div class="username"><input type="text" class="user-input" placeholder="image url" /></div>
+                <div className='border p-3 border-gray-500'>
+                <div class="fields1 text-black">
+                    <h1 className='text-3xl'>Payment Details</h1>
+                    <p>Complete your purchase by providing your Payment info </p>
+                    <br />
+                    <br />
+                    <p>Enter Email address</p>
                     <div class="username"><input type="email" class="user-input" placeholder="email" /></div>
-                    <div class="password"><input type="password" class="pass-input" placeholder="password" /></div>
+                  
+                    <p>Enter Card Number</p>
+                    <div class="username"><input type="text" class="user-input" placeholder="Card number" /></div>
+                    <p>Enter Cardholder name</p>
+                    <div class="username"><input type="text" class="user-input" placeholder="Cardholder" /></div>
+                    <p>Enter VAT number</p>
+                    <div class="username"><input type="text" class="user-input" placeholder="VAT number" /></div>
+                    <p>Enter Billing address</p>
+                    <div className='flex gap-3'>
+
+                    <div class="username"><input type="text" class="user-input" placeholder="Address" /></div>
+                    <div class="username"><input type="text" class="user-input" placeholder="City" /></div>
+                    </div>
+                    <div className='flex gap-3'>
+                        
+                    <div class="username"><input type="text" class="user-input" placeholder="ZIP" /></div>
+                    <div class="username"><input type="text" class="user-input" placeholder="State" /></div>
+                    </div>
+                    <div className='flex gap-3'>
+                        
+                    <div class="username"><input type="text" class="user-input" placeholder="Country" /></div>
+                    
+                    </div>
+                    
+
+               
                 </div>
-                <button class="signin-button">Register</button>
-                <div class="link">
-                    <a href="#">Already have an account? </a> <br /> <a href="#">Log In</a>
+                <button class="signin-button">Proceed to payment </button>
                 </div>
              </div>
             
+        </div>
         </div>
     );
 };
