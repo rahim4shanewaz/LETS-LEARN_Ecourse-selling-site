9eac94787acc89aace193dae12c76d5398f71502
rahim4shanewaz <soumiq444@gmail.com>
2022-10-26 18:19:40 +0600

    router setup

--- src/components/checkOut/CheckOut.js
+++ src/components/checkOut/CheckOut.js
@@ -3,8 +3,50 @@ import './CheckOut.css'
 
 const CheckOut = () => {
     return (
-        <div>
-            this is CheckOut page
+        <div className='flex gap-5 justify-center items-center'>
+            
+            <div class="login-div1">
+  
+                <div class="title">Register First</div>
+                
+                <div class="fields1">
+                    <div class="username">
+                        
+                        <input type="text" class="user-input" placeholder="name" />
+                        </div>
+                    <div class="username"><input type="text" class="user-input" placeholder="image url" /></div>
+                    <div class="username"><input type="email" class="user-input" placeholder="email" /></div>
+                    <div class="password"><input type="password" class="pass-input" placeholder="password" /></div>
+                </div>
+                <button class="signin-button">Register</button>
+                <div class="link">
+                    <a href="#">Already have an account? </a> <br /> <a href="#">Log In</a>
+                </div>
+             </div>
+
+
+
+             
+             <div class="login-div1">
+  
+                <div class="title">Register First</div>
+                
+                <div class="fields1">
+                <p>Enter your present address</p>
+                    <div class="username"><input type="text" class="user-input" placeholder="Present address" /></div>
+                    <p>Enter your Full name</p>
+                    <div class="username"><input type="text" class="user-input" placeholder="Full name" /></div>
+                    <p>Enter your Mobile number</p>
+                    <div class="username"><input type="text" class="user-input" placeholder="mobile number" /></div>
+                    <div class="username"><input type="text" class="user-input" placeholder="image url" /></div>
+                    <div class="username"><input type="email" class="user-input" placeholder="email" /></div>
+                    <div class="password"><input type="password" class="pass-input" placeholder="password" /></div>
+                </div>
+                <button class="signin-button">Register</button>
+                <div class="link">
+                    <a href="#">Already have an account? </a> <br /> <a href="#">Log In</a>
+                </div>
+             </div>
             
         </div>
     );
--- src/components/notFound/NotFoundPage.js
+++ src/components/notFound/NotFoundPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './NotFoundPage.css'
 
 const NotFoundPage = () => {
@@ -8,6 +9,9 @@ const NotFoundPage = () => {
          <h1 className='text-6xl font-bold text-red-600'> 404 </h1>
          <h1 className='text-3xl'>Opps !!</h1>
          <h1 className='text-2xl'>This Page Not Found</h1>
+         <div className='m-5'>
+         <NavLink className={'hover:bg-white hover:text-black'} to='/'><button>Go Back to our website</button></NavLink>
+         </div>
 
         </div>
      </div>
